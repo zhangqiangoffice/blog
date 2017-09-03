e72c9aaa5fabc5bbbf636bc6c798f9af7146e501
inkun <zhangqiangoffice@163.com>
2017-09-03 12:33:53 +0800

    feat:配置数据库连接

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var swig = require('swig')
+var mongoose = require('mongoose')
 
 app.use('/public', express.static(__dirname + '/public'))
 app.engine('html', swig.renderFile)
@@ -13,8 +14,14 @@ app.use('/admin', require('./routers/admin'))
 app.use('/api', require('./routers/api'))
 app.use('/main', require('./routers/main'))
 
+mongoose.connect('mongodb://localhost:27018/blog', err => {
+  if (err) {
+    console.log('数据库连接失败')
+  } else {
+    console.log('数据库连接成功')
+    app.listen(8081, () => {
+      console.log('server is listening at 8081 ...')
+    })
 
-
-app.listen(8081, () => {
-  console.log('server is listening at 8081 ...')
+  }
 })
