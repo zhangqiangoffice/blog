b2f1c1b70482d822069a9b6e2fc0b9977198c861
inkun <zhangqiangoffice@163.com>
2017-09-03 11:36:54 +0800

    feat: 配置模版引擎

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require('express')
 var app = express()
+var swig = require('swig')
+
+app.engine('html', swig.renderFile)
+app.set('views', './views')
+app.set('view engine', html)
+
+swig.setDefaults({cache: false})
+
+
 
 app.listen(8081, () => {
   console.log('server is listening at 8081 ...')
