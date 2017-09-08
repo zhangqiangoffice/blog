b19f280651c16b288a1ab862cb42eaefbc1f1a24
Quentin Zhang <quentinzhang@zillionsource.com>
2017-09-08 15:09:04 +0800

    feat: 前台内容分页展示

--- routers/main.js
+++ routers/main.js
@@ -1,13 +1,34 @@
 var express = require('express')
 var router = express.Router()
 var Category = require('../models/Category')
+var Content = require('../models/Content')
 
 router.get('/', (req, res, next) => {
+  
+  var data = {
+    userInfo: req.userInfo,
+    categories: [],
+    count: 0,
+    page: Number(req.query.page || 1),
+    limit: 2,
+    pages: 0
+  }
+  
   Category.find().then(categories => {
-    res.render('main/index', {
-      userInfo: req.userInfo,
-      categories: categories,
-    })
+    
+    data.categories = categories
+    return Content.count()
+  }).then(count => {
+    data.count = count
+    data.pages = Math.ceil(count / data.limit)
+    data.page = Math.min(data.page, data.pages)
+    data.page = Math.max(data.page, 1)
+    var skip = (data.page - 1) * data.limit
+    
+    return Content.find().sort({_id: -1}).limit(data.limit).skip(skip).populate(['category', 'user']).sort({addTime: -1})
+  }).then((contents)=> {
+    data.contents = contents
+    res.render('main/index', data)
   })
 })
 
