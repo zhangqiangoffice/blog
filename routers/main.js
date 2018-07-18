0d025f23b908c6f0e791a641c20cfc79e52a8f4f
Quentin Zhang <quentinzhang@zillionsource.com>
2018-07-18 16:55:41 +0800

    feat: 使用marked展示文章

--- routers/main.js
+++ routers/main.js
@@ -2,15 +2,16 @@ var express = require('express')
 var router = express.Router()
 var Category = require('../models/Category')
 var Content = require('../models/Content')
+const marked = require('marked')
 
 var data
 
 router.use((req, res, next) => {
   data = {
     userInfo: req.userInfo,
-    categories: [],
+    categories: []
   }
-  
+
   Category.find().then(categories => {
     data.categories = categories
     next()
@@ -18,7 +19,6 @@ router.use((req, res, next) => {
 })
 
 router.get('/', (req, res, next) => {
-  
   data.category = req.query.category || ''
   data.count = 0
   data.page = Number(req.query.page || 1)
@@ -29,16 +29,16 @@ router.get('/', (req, res, next) => {
   if (data.category) {
     where.category = data.category
   }
-  
+
   Content.where(where).count().then(count => {
     data.count = count
     data.pages = Math.ceil(count / data.limit)
     data.page = Math.min(data.page, data.pages)
     data.page = Math.max(data.page, 1)
     var skip = (data.page - 1) * data.limit
-    
+
     return Content.where(where).find().sort({_id: -1}).limit(data.limit).skip(skip).populate(['category', 'user']).sort({addTime: -1})
-  }).then((contents)=> {
+  }).then((contents) => {
     data.contents = contents
     res.render('main/index', data)
   })
@@ -52,7 +52,7 @@ router.get('/view', (req, res) => {
 
     content.views++
     content.save()
-    
+    data.textHtmlStr = marked(content.content)
     res.render('main/view', data)
   })
 })
