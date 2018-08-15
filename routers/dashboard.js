12c64b5ad395650393c45967af8c3beadeecd669
Zhang Qiang with ubuntu <zhangqiangoffice@163.com>
2018-08-15 18:05:53 +0800

    feat: 编辑文章

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -217,270 +217,32 @@ router.post('/contents', (req, res) => {
   })
 })
 
-router.get('/category/add', (req, res) => {
-  res.render('admin/category_add', {
-    userInfo: req.userInfo
-  })
-})
+router.put('/contents/:content_id', (req, res) => {
+  const id = req.content.id || ''
+  const { category, title, description, content } = req.body
 
-router.post('/category/add', (req, res) => {
-  var name = req.body.name || ''
-  if (name === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '名称不能为空！'
-    })
-    return
+  if (!category || !title || !description || !content) {
+    responseData.code = 1
+    responseData.message = '文章内容字段不全'
+    res.json(responseData)
   }
 
-  Category.findOne({name: name}).then(rs => {
-    if (rs) {
-      res.render('admin/error', {
-        userInfo: req.userInfo,
-        message: '分类已经存在了！'
-      })
-      return Promise.reject()
-    } else {
-      return new Category({
-        name: name
-      }).save()
+  Content.findOne({ _id: id }, (err, result) => {
+    if (err) {
+      res.json(handleError(err))
     }
-  }).then(newCategory => {
-    res.render('admin/success', {
-      userInfo: req.userInfo,
-      message: '分类保存成功！',
-      url: '/admin/category'
-    })
-  })
-})
-
-router.get('/category/edit', (req, res) => {
-  var id = req.query.id || ''
-
-  Category.findOne({_id: id}).then(category => {
-    if (!category) {
-      res.render('admin/error', {
-        userInfo: req.userInfo,
-        message: '分类信息不存在！'
-      })
+    if (!result) {
+      responseData.code = 2
+      responseData.message = '文章不存在！'
+      res.json(responseData)
     } else {
-      res.render('admin/category_edit', {
-        userInfo: req.userInfo,
-        category: category
+      Content.update({ _id: id }, { category, title, description, content, addTime: new Date() }, (err, result) => {
+        if (err) {
+          res.json(handleError(err))
+        }
+        res.json(responseData)
       })
     }
-  })
-})
-
-router.post('/category/edit', (req, res) => {
-  var id = req.query.id || ''
-  var name = req.body.name || ''
-
-  if (name === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '名称不能为空！'
-    })
-    return
-  }
-
-  Category.findOne({_id: id}).then(category => {
-    if (!category) {
-      res.render('admin/error', {
-        userInfo: req.userInfo,
-        message: '分类信息不存在！'
-      })
-    } else {
-      if (category.name === name) {
-        res.render('admin/success', {
-          userInfo: req.userInfo,
-          message: '分类信息修改成功！',
-          url: '/admin/category'
-        })
-        return Promise.reject()
-      } else {
-        return Category.findOne({
-          _id: {$ne: id},
-          name: name
-        })
-      }
-    }
-  }).then(sameCategory => {
-    if (sameCategory) {
-      res.render('admin/error', {
-        userInfo: req.userInfo,
-        message: '数据库中已经存在同名分类了！'
-      })
-      return Promise.reject()
-    } else {
-      return Category.update({
-        _id: id
-      }, {
-        name: name
-      })
-    }
-  }).then(() => {
-    res.render('admin/success', {
-      userInfo: req.userInfo,
-      message: '分类信息修改成功！',
-      url: '/admin/category'
-    })
-  })
-})
-
-router.get('/category/delete', (req, res) => {
-  var id = req.query.id || ''
-
-  Category.remove({_id: id}).then(() => {
-    res.render('admin/success', {
-      userInfo: req.userInfo,
-      message: '分类删除成功'
-    })
-  })
-})
-
-router.get('/content', (req, res) => {
-  var page = Number(req.query.page) || 1
-  var limit = 10
-  var pages = 0
-
-  Content.count().then(count => {
-    pages = Math.ceil(count / limit)
-    page = Math.min(page, pages)
-    page = Math.max(page, 1)
-    var skip = (page - 1) * limit
-
-    Content.find().sort({_id: -1}).limit(limit).skip(skip).populate(['category', 'user']).then(contents => {
-      res.render('admin/content_index', {
-        userInfo: req.userInfo,
-        contents: contents,
-        page: page,
-        count: count,
-        limit: limit,
-        pages: pages
-      })
-    })
-  })
-})
-
-router.get('/content/add', (req, res) => {
-  Category.find().then(categories => {
-    res.render('admin/content_add', {
-      userInfo: req.userInfo,
-      categories: categories
-    })
-  })
-})
-
-router.post('/content/add', (req, res) => {
-  if (req.body.category === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '内容分类不能为空！'
-    })
-  }
-
-  if (req.body.category === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '内容标题不能为空！'
-    })
-  }
-
-  new Content({
-    category: req.body.category,
-    title: req.body.title,
-    user: req.userInfo._id.toString(),
-    description: req.body.description,
-    content: req.body.content
-  }).save().then(rs => {
-    res.render('admin/success', {
-      userInfo: req.userInfo,
-      message: '内容保存成功！',
-      url: '/admin/content'
-    })
-  })
-})
-
-router.get('/content/edit', (req, res) => {
-  var id = req.query.id || ''
-  var categories = []
-
-  Category.find().then(rs => {
-    categories = rs
-    return Content.findOne({_id: id}).populate('category')
-  }).then(content => {
-    if (!content) {
-      res.render('admin/error', {
-        userInfo: req.userInfo,
-        message: '指定内容不存在！'
-      })
-    } else {
-      res.render('admin/content_edit', {
-        userInfo: req.userInfo,
-        content: content,
-        categories: categories
-      })
-    }
-  })
-})
-
-router.post('/content/edit', (req, res) => {
-  var id = req.query.id || ''
-
-  if (id === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '内容ID不能为空！'
-    })
-  }
-
-  if (req.body.category === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '内容分类不能为空！'
-    })
-  }
-
-  if (req.body.category === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '内容标题不能为空！'
-    })
-  }
-
-  Content.update({
-    _id: id
-  }, {
-    category: req.body.category,
-    title: req.body.title,
-    description: req.body.description,
-    content: req.body.content,
-    addTime: new Date()
-  }).then(() => {
-    res.render('admin/success', {
-      userInfo: req.userInfo,
-      message: '内容修改成功！',
-      url: '/admin/content'
-    })
-  })
-})
-
-router.get('/content/delete', (req, res) => {
-  var id = req.query.id || ''
-
-  if (id === '') {
-    res.render('admin/error', {
-      userInfo: req.userInfo,
-      message: '内容ID不能为空！'
-    })
-  }
-
-  Content.remove({_id: id}).then(() => {
-    res.render('admin/success', {
-      userInfo: req.userInfo,
-      message: '内容删除成功'
-    })
   })
 })
 
